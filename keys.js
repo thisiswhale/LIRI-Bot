09fee87a5aa69f626d87065758808d3498e800dc
William Leung <William Leung>
2017-05-04 14:31:24 -0700

    Made my files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '7A7f9GpmlRSUqr5uM5Sj4bcSa',
+  consumer_secret: 'uWCeGIuFx4cq8Um0TkFG8CsxIUZ4UsXqChKXysA6ZtYoZSV76g',
+  access_token_key: '860242502044893184-XS7xQGJ9RLG6LLusainovO23GwJyqgy',
+  access_token_secret: 'QJigjuUpHrj03FP2tforvUHc7izKESr9mCHT4anKmgBlh',
+}
